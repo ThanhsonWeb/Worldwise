fa124f41f987a4c59b1383455e20ccf3974d6389
ThanhsonWeb <sondeptroi@gmail.com>
2026-05-21 10:06:13 +0700

    responsive for Nav and homepage

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,21 +1,26 @@
 import logo from "../assets/img/logo.png";
 import { NavLink, Link } from "react-router-dom";
-
+import barIcon from "../assets/icon/bar.svg";
 function PageNav() {
 	return (
 		<div className="flex items-center justify-between ">
 			<Link to="/">
 				<img src={logo} alt="logo" className="h-16" />
 			</Link>
-			<nav>
+
+			<button>
+				<img src={barIcon} alt="barIcon" className="h-17" />
+			</button>
+
+			<nav className="md:block hidden">
 				<ul className="flex gap-5 text-2xl text-amber-50 ">
-					<li className="px-4 py-2" >
+					<li className="px-4 py-2">
 						<NavLink to="/pricing">Pricing</NavLink>
 					</li>
-					<li className="px-4 py-2" >
+					<li className="px-4 py-2">
 						<NavLink to="/product">Product</NavLink>
 					</li>
-					<li className="px-4 py-2" >
+					<li className="px-4 py-2">
 						<NavLink to="/login">Login</NavLink>
 					</li>
 				</ul>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,14 +3,14 @@ import LinkButton from "../ui/LinkButton";
 
 function HomePage() {
 	return (
-		<div className="homepage text-center m-6 px-[5rem] py-[2.5rem] text-white  ">
+		<div className="homepage text-center m-6 md:px-[5rem] px-[2rem] py-[2.5rem] text-white  ">
 			<PageNav />
-			<h1 className="text-5xl font-semibold my-[2rem]">
+			<h1 className="md:text-5xl text-3xl font-semibold my-[2rem]">
 				You travel the world.
 				<br />
 				WorldWise keeps track of your adventures.
 			</h1>
-			<h2 className="text-2xl font-semibold text-stone-300 mb-10">
+			<h2 className="md:text-2xl text-xl font-semibold text-stone-300 mb-10">
 				A world map that tracks your footsteps into every city you can think of.
 				Never forget your wonderful experiences, and show your friends how you
 				have wandered the world.
